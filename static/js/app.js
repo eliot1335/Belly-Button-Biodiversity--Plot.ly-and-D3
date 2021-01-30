efbca82cc10e52a40f9b891bedcd22cd3eda351e
Eliot Chern <eliotchern@gmail.com>
2021-01-30 09:54:16 -0800

    test

--- static/js/app.js
+++ static/js/app.js
@@ -1,1 +1,8 @@
+// Fetch the JSON data and console log it
+d3.json("../samples.json").then(function(data) {
+  console.log(data);
+});
 
+// // Promise Pending
+// const dataPromise = d3.json(url);
+// console.log("Data Promise: ", dataPromise);
