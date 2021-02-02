7eb52b3cb27da07deeabd50896383b9ec51ddeea
Eliot Chern <eliotchern@gmail.com>
2021-02-02 09:00:18 -0800

    Top 10 iso

--- static/js/app.js
+++ static/js/app.js
@@ -12,29 +12,25 @@
    5. Display each key-value pair from the metadata JSON object somewhere on the page.
    6. Update all of the plots any time that a new sample is selected.
   */
-
-d3.json("../samples.json").then(function(data) {
-  console.log(data);
-});
-
-
-// Populate dropdown menu
-d3.json("../samples.json").then(function(jsonData){
-
-  var select = document.getElementById("selDataset");
-  var options = jsonData["names"]
-  for (var i = 0; i < options.length; i++) {
-    var opt =options[i];
-    var el = document.createElement("option");
-    el.textContent = opt;
-    el.value = opt;
-    select.appendChild(el);
-  }
-})
-
+ 
+// d3.json("../samples.json").then(function(jsonData) {
+//   console.log(jsonData);
+// });
 // Initializes the page
 function init() {
   d3.json("../samples.json").then(function(jsonData) {
+
+    // Populate dropdown
+    var select = document.getElementById("selDataset");
+    var options = jsonData["names"]
+    for (var i = 0; i < options.length; i++) {
+      var opt =options[i];
+      var el = document.createElement("option");
+      el.textContent = opt;
+      el.value = opt;
+      select.appendChild(el);
+    };
+
     var sampleId = jsonData.names[0];
     buildDisplays(sampleId);
   })
@@ -51,15 +47,15 @@ function optionChanged(sampleId) {
 function buildDisplays(sampleId) {
   // Read in json file
   d3.json("../samples.json").then(function(jsonData) {
-    var samplesFilter = jsonData["samples"].filter(item => item["id"] == sampleId);
-    var metaDataFilter = jsonData["metadata"].filter(row => row["id"] == sampleId);
+    var sFilter = jsonData["samples"].filter(row => row["id"] == sampleId);
+    var mFilter = jsonData["metadata"].filter(row => row["id"] == sampleId);
 
     // Set variables for bubble chart
-    var xBubble = samplesFilter[0].otu_ids;
-    var yBubble = samplesFilter[0].sample_values;
-    var sizeBubble = samplesFilter[0].sample_values;
-    var colorBubble = samplesFilter[0].otu_ids;
-    var textBubble = samplesFilter[0].otu_labels;
+    var xBubble = sFilter[0].otu_ids;
+    var yBubble = sFilter[0].sample_values;
+    var sizeBubble = sFilter[0].sample_values;
+    var colorBubble = sFilter[0].otu_ids;
+    var textBubble = sFilter[0].otu_labels;
 
     // Set bubble chart parameters
     var bubble = {
@@ -69,7 +65,8 @@ function buildDisplays(sampleId) {
       mode: "markers",
       marker: {
         size: sizeBubble,
-        color: colorBubble
+        color: colorBubble,
+        colorscale: "Earth"
       }
     };
 
@@ -86,9 +83,18 @@ function buildDisplays(sampleId) {
 
     //==========================================
     // Set Variables for Bar Chart
-    var xBar = samplesFilter[0].sample_values
-    var yBar = samplesFilter[0].otu_ids
-    var textBar = samplesFilter[0].otu_labels
+    
+    // top10Data = samplesFilter[0].sort(compareValues("sample_values", "desc")).slice(0, 10);
+    // console.log(top10Data);
+
+
+
+    var xBar = sFilter[0].sample_values.slice(0, 10).reverse();
+    console.log(xBar);
+    // var xBarTop10 = xBar.sort((firstNum, secondNum) => secondNum - firstNum).slice(0, 10);
+    // console.log(xBar);
+    var yBar = `${sFilter[0].otu_ids}`;
+    var textBar = sFilter[0].otu_labels.slice(0, 10);
 
     // Set bubble chart parameters
     var bar = {
@@ -115,7 +121,7 @@ function buildDisplays(sampleId) {
     // Display metaData
     var metadataPanel = d3.select("#sample-metadata");
     metadataPanel.html("");
-    Object.entries(metaDataFilter[0]).forEach(([key, value]) => {
+    Object.entries(mFilter[0]).forEach(([key, value]) => {
       metadataPanel.append("h6").text(`${key}: ${value}`);
     });
   });
